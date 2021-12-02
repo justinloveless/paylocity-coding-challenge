e620382bf9f2c3f68824365398ad09bb19249ea9
Justin Loveless <justin.loveless@hp.com>
2021-12-01 19:48:03 -0600

    Add Primary Keys to models
    Forgot about the primary keys. Ooops!

--- PCC/API/Models/Dependant.cs
+++ PCC/API/Models/Dependant.cs
@@ -2,6 +2,7 @@
 
 public class Dependant
 {
+    public int DependantId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
 }
--- PCC/API/Models/Employee.cs
+++ PCC/API/Models/Employee.cs
@@ -2,6 +2,7 @@
 
 public class Employee
 {
+    public int EmployeeId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public decimal Salary { get; set; }
