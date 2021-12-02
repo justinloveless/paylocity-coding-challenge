f82a798b144a808e776e259361d5a758fed678e4
Justin Loveless <justin.loveless@hp.com>
2021-12-01 19:34:23 -0600

    Add CORS

--- PCC/API/Program.cs
+++ PCC/API/Program.cs
@@ -6,8 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(optBuilder => optBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
