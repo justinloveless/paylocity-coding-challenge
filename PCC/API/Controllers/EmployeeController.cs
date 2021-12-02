f02ce85e6f634430774241418270159d38629532
Justin Loveless <justin.loveless@hp.com>
2021-12-01 23:59:50 -0600

    Scaffold out Controller

--- PCC/API/Controllers/EmployeeController.cs
+++ PCC/API/Controllers/EmployeeController.cs
@@ -1,6 +1,23 @@
-﻿namespace API.Controllers;
+﻿using API.Interfaces;
+
+namespace API.Controllers;
 
 public class EmployeeController : BaseApiController
 {
+    private readonly IEmployeeService _employeeService;
+    private readonly IDependantService _dependantService;
+
+    public EmployeeController(IEmployeeService employeeService, IDependantService dependantService)
+    {
+        _employeeService = employeeService;
+        _dependantService = dependantService;
+    }
     
+    // TODO: Get All
+    // TODO: Hire Employee
+    // TODO: Fire employee
+    // TODO: Find employee via query
+    // TODO: Update employee salary
+    // TODO: add dependant to employee
+    // TODO: remove dependant from employee
 }
