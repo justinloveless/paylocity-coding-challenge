e3b08b49a10438c10725aa60262a338b129ed702
Justin <justinlovelessx@gmail.com>
2021-12-03 00:47:58 -0600

    basic UI blocked out

--- PCC/API/ClientApp/src/app/app.module.ts
+++ PCC/API/ClientApp/src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatDialogModule} from "@angular/material/dialog";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {TooltipModule} from "ngx-bootstrap/tooltip";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    // BrowserModule,
+    // AppRoutingModule,
+    // BrowserAnimationsModule,
+    // MatExpansionModule,
+    // MatButtonModule,
+    // MatFormFieldModule,
+    // FormsModule,
+    // MatInputModule,
+    // MatDialogModule,
+    // HttpClientModule,
+    // MatToolbarModule,
+
+
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatExpansionModule,
+    TooltipModule.forRoot(),
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
