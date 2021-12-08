4a07369653809254730684c819f8450535f9f81d
Justin Loveless <justin.loveless@hp.com>
2021-12-08 00:40:47 -0600

    Implement add and remove dependant in API test UI

--- PCC/API/ClientApp/src/app/components/api-tests/remove-dependant/remove-dependant.component.ts
+++ PCC/API/ClientApp/src/app/components/api-tests/remove-dependant/remove-dependant.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {EmployeeDependant} from "../../../models/employee-dependant";
 import {RelationshipTypes} from "../../../models/relationship-types";
+import {DependantService} from "../../../services/dependant.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-remove-dependant',
@@ -12,13 +14,40 @@ export class RemoveDependantComponent implements OnInit {
     dependantId: 0, employeeId: 0, firstName: "", lastName: "", relationship: RelationshipTypes.child
 
   }
-  constructor() { }
+  isLoading = false;
+
+  constructor(private dependantService: DependantService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  reset(){
+    this.isLoading = false;
+    this.dependantToRemove = {
+      dependantId: 0, employeeId: 0, firstName: "", lastName: "", relationship: RelationshipTypes.child
+    }
+  }
+
+  notify(message: string){
+    this.snackBar.open(message, "Dismiss", {duration: 2000})
+  }
+
   removeDependant() {
-    alert("Not implemented yet")
+    this.isLoading = true;
+    this.dependantService.removeDependant(this.dependantToRemove.dependantId, this.dependantToRemove.employeeId)
+      .subscribe({
+        next: () => {
+          const removedId = this.dependantToRemove.dependantId.toString(); // saving old value as it will be reset
+          this.reset();
+          this.notify(`Dependant ${removedId} removed from employee
+          ${this.dependantToRemove.employeeId}`)
+        },
+        error: err => {
+          this.reset()
+          this.notify(`Unable to remove dependant from employee`)
+        }
+      })
+
   }
 
 }
